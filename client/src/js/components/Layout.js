0960e23445f5eaa9bbbd25bb4e0b90d5065b8fe9
John P. Willis <jpw@coherent-logic.com>
2017-02-14 12:16:33 -0700

    Initial commit

---
+++ client/src/js/components/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+
+    constructor() {
+	super();
+	this.state = {
+	    title: "Welcome",
+	};
+    }
+    
+    render() {
+	return(
+	    <div>	    
+	    {this.state.title}
+	    </div>
+	);
+    }
+
+}
